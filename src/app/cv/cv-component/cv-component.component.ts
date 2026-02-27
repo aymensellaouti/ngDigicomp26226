f4f1ff16588dff6bc8e226dc2a62b6b880a04a97
aymensellaouti <aymen.sellaouti@gmail.com>
2026-02-27 10:33:33 +0100

    exmple pipe et custom pipe et custom directive

--- src/app/cv/cv-component/cv-component.component.ts
+++ src/app/cv/cv-component/cv-component.component.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { Cv } from '../model/cv.model';
 import { CvList } from "../cv-list/cv-list.component";
 import { CvCard } from "../cv-card/cv-card.component";
+import { DatePipe, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-cv-component',
-  imports: [CvList, CvCard],
+  imports: [CvList, CvCard, DatePipe, UpperCasePipe],
   templateUrl: './cv-component.component.html',
   styleUrl: './cv-component.component.css',
 })
@@ -13,9 +14,10 @@ export class CvComponent {
   // state
   cvs = signal([
     new Cv(1, 'Guemara', 'jules', 'Bibliothècaire', '1234', 20, 'rotating_card_profile3.png'),
-    new Cv(2, 'Khribech', 'Anass', 'Bibliothècaire', '1234', 20, 'rotating_card_profile2.png'),
+    new Cv(2, 'Khribech', 'Anass', 'Bibliothècaire', '1234', 20, ''),
+    new Cv(3, 'Sellaouti', 'Aymen', 'Bibliothècaire', '1234', 20, '            '),
   ]);
-
+  today = new Date();
   selectedCv = signal<Cv | null>(null);
   updateSelectedCv(cv: Cv) {
     this.selectedCv.set(cv);
