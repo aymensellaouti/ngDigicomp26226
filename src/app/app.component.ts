08c8f74daea5f49d740249d6b57b5d3fc2e10820
aymensellaouti <aymen.sellaouti@gmail.com>
2026-02-27 14:14:35 +0100

    mise en place du routeur

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,10 @@
 import { Component } from '@angular/core';
-import { First } from "./first/first.component";
-import { FirstSignalComponent } from "./signals/first-signal/first-signal.component";
-import { TtcComponent } from "./signals/ttc/ttc.component";
-import { Pere } from "./commInterComposants/pere/pere.component";
-import { CvComponent } from "./cv/cv-component/cv-component.component";
-import { ExemplesStyle } from "./directives/exemples-style/exemples-style.component";
-import { MiniWordComponent } from "./directives/mini-word/mini-word.component";
-import { Lampe } from "./directives/lampe/lampe.component";
-import { TodoComponent } from "./todo/todo/todo.component";
+import { RouterOutlet } from "@angular/router";
+import { Navbar } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [MiniWordComponent, Lampe, CvComponent, ExemplesStyle, TodoComponent],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { First } from './first/first.component';
+import { CvComponent } from './cv/cv-component/cv-component.component';
+import { Todo } from './todo/model/todo';
+import { MiniWordComponent } from './directives/mini-word/mini-word.component';
+import { Color } from './components/color/color.component';
+import { TodoComponent } from './todo/todo/todo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  //cv
+  { path: '', component: First },
+  { path: 'cv', component: CvComponent },
+  { path: 'todo', component: TodoComponent },
+  { path: 'word', component: MiniWordComponent },
+  { path: 'color', component: Color },
+];
