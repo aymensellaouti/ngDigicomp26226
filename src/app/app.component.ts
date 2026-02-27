7949a12746c74242940fa4b7179c2cc93808833d
aymensellaouti <aymen.sellaouti@gmail.com>
2026-02-27 15:47:38 +0100

    exemple testForm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from "@angular/router";
 import { Navbar } from "./components/navbar/navbar.component";
+import { TestForm } from "./components/test-form/test-form.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, TestForm],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
