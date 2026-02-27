275184642c22720c2688a63c2a5001b8e16cf3ea
aymensellaouti <aymen.sellaouti@gmail.com>
2026-02-27 17:04:26 +0100

    toSignal

--- src/app/cv/details-cv/details-cv.component.ts
+++ src/app/cv/details-cv/details-cv.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject, signal } from "@angular/core";
+import { Component, inject, Signal, signal } from "@angular/core";
 import { Cv } from "../model/cv.model";
 import { DefaultImagePipe } from "../../pipes/default-image.pipe";
 import { ActivatedRoute, Router } from "@angular/router";
 import { CvService } from "../services/cv.service";
 import { APP_ROUTES } from "../../config/app-routes.config";
+import { toSignal } from "@angular/core/rxjs-interop";
 
 @Component({
   selector: 'app-details-cv',
@@ -15,18 +16,22 @@ export class DetailsCvComponent {
   acr = inject(ActivatedRoute);
   cvService = inject(CvService);
   router = inject(Router);
-  cv = signal<Cv | null>(null);
+  cv: Signal<Cv | null>;
 
   constructor() {
     const id = this.acr.snapshot.params['id'];
+    this.cv = toSignal(this.cvService.getCvById(id), {
+      initialValue: null
+    });
+    if (!this.cv()) this.router.navigate([APP_ROUTES.cv]);
     //this.cv.set(this.cvService.findCvById(id));
     //if(!this.cv()) this.router.navigate([APP_ROUTES.cv]);
-    this.cvService.getCvById(id).subscribe({
-      next: cv => {this.cv.set(cv)},
-      error: (e) => {
-        this.router.navigate([APP_ROUTES.cv]);
-      }
-    })
+    // this.cvService.getCvById(id).subscribe({
+    //   next: cv => {this.cv.set(cv)},
+    //   error: (e) => {
+    //     this.router.navigate([APP_ROUTES.cv]);
+    //   }
+    // })
   }
 
   deleteCv() {
